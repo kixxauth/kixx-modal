272803f4fa0ac65e06f022d722d541bda2c08f24
Kristoffer Walker <kris@kixx.name>
2013-11-15 21:20:32 -0500

    Start of javascript
     On branch master
    
    	modified:   examples.html
    	modified:   jquery.kixx-modal.js

--- jquery.kixx-modal.js
+++ jquery.kixx-modal.js
@@ -1,2 +1,37 @@
-(function () {
-}());
+(function (window, document, $) {
+
+  function createModalDeck(opts) {
+    var self = {}
+      , currentlyOpen = null
+
+    self.open = function (id, callback) {
+      id = trimId(id);
+
+      if (id == currentlyOpen) {
+        if ($.isFunction(callback)) {
+          callback();
+        }
+        return this;
+      }
+
+      var $modal
+
+      $('#'+ currentlyOpen).kixxModal('close', opts, function () {
+        $modal.kixxModal('open', opts, function () {
+          if ($.isFunction(callback)) {
+            callback();
+          }
+        });
+      });
+
+      return this;
+    };
+
+    return self;
+  }
+
+  function trimId(str) {
+    return str.replace(/^#/, '');
+  }
+
+}(window, document, jQuery));
